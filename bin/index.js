c06b7f0d9a8a5816ff870e7ed3c6bed8c1e2ea15
anwachuku15 <anwachuku15@gmail.com>
2020-04-28 12:14:47 -0400

    laid out plan for app

--- bin/index.js
+++ bin/index.js
@@ -20,6 +20,16 @@ const greeting = () => {
 
 const app = () => {
     greeting()
+
+    // Run app until user exits - WHILE LOOP
+        // Navigate thru app?? -- IF STATEMENTS
+            // allow user to search for a book - display 5 results
+
+            // allow user to add a book to their reading list
+
+            // allow user to view their reading list
+
+            // allow user to exit app -- exit out of while loop
 }
 
 app()
