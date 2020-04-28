9dec0a4da3926f859840e7e0e54d6751415fe419
anwachuku15 <anwachuku15@gmail.com>
2020-04-28 12:10:48 -0400

    installed command line styling & initialized app

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,25 @@
 #!/usr/bin/env node
-console.log('hello')
+const term = require('terminal-kit').terminal
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+const styles = {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'round',
+    borderColor: 'magenta',
+    backgroundColor: 'black'
+}
+
+const greeting = () => {
+    const greetingMsg = chalk.redBright.bold('Welcome to Your Book Club!')
+    const msgBox = boxen(greetingMsg, styles)
+    console.log(msgBox)
+    term.bold.red("Let's get started!\n\n")
+}
+
+const app = () => {
+    greeting()
+}
+
+app()
